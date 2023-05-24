ef09e0514a0c960c3f7d6baff804e5c3c018f79e
wojtekczajka <w.czajka@gmail.com>
2023-05-24 11:03:08 +0200

    api url update

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ if (token) {
 const app = createApp(App);
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'https://ti4r36gvwlegcokae4ofeivnva0hwiqn.lambda-url.eu-north-1.on.aws';  // the FastAPI backend
+axios.defaults.baseURL = 'https://fastapi-server-ezey.onrender.com';  // the FastAPI backend
 axios.interceptors.request.use(function(config) {
   // change the url scheme from http to https
   config.url = config.url.replace('http://', 'https://')
